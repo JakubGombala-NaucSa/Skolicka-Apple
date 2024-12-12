821af89870c4907ab36d849bf0627537fb8b1709
JakubGombala-NaucSa <spam.ucet4713@gmail.com>
2024-12-12 11:18:23 +0100

    Update hodina5.py

--- hodina5.py
+++ hodina5.py
@@ -12,8 +12,18 @@ def Start():
     vytvor_gulicky()
 
 def vytvor_gulicky():
+    posun = -10
     for i in range(10):
-        gulicka = platno.create_oval(10 + i*20, 10, 30 + i*20, 30, fill="red")
+        posun += 20
+        gulicka = platno.create_oval(10 + posun, 10, 30 + posun, 30, fill="red")
         lopticky.append(gulicka)
+        posun += 10
+
+def Hra():
+    for lopticka in lopticky:
+        platno.move(lopticka, 0, 10)
+    platno.after(100, Hra)
+        
 
 Start()
+Hra()
